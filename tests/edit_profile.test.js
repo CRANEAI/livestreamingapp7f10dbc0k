6f143ecfe0fc46de0c223009fb215fbde6bf5f8d
Elly <elly@mg.crane.ai>
2018-10-31 23:15:18 -0400

    added tests/edit_profile updated project details in new api

---
+++ tests/edit_profile.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - edit profile UNIT TESTS
+ * Application Client
+ * Live Streaming App
+*/
+
+/* composite view has Label located at the bottom center of the view, text needs identified */
+it("composite view has Label located at the bottom center of the view, text needs identified", () => {
+  /* ToDo developer needs to complete unit test 
+               composite view has Label located at the bottom center of the view, text needs identified
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})

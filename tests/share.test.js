c8ab143e84a62be41ad28da166b0f59db1767a19
Elly <elly@mg.crane.ai>
2018-10-31 23:22:45 -0400

    added tests/share updated project details in new api

---
+++ tests/share.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - share UNIT TESTS
+ * Application Client
+ * Live Streaming App
+*/
+
+/* history view has a Label located at the mid-bottom left of the view with text k" */
+it('history view has a Label located at the mid-bottom left of the view with text k"', () => {
+  /* ToDo developer needs to complete unit test 
+               history view has a Label located at the mid-bottom left of the view with text k"
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})

c17661357faaaa737181a8beacf575d6d4b7ffa1
Elly <elly@mg.crane.ai>
2018-10-31 23:15:22 -0400

    added tests/notifications updated project details in new api

---
+++ tests/notifications.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - notifications UNIT TESTS
+ * Application Client
+ * Live Streaming App
+*/
+
+/* the notifications view has a close icon located at the very-top right of the view which on click will trigger the close() action */
+it("the notifications view has a close icon located at the very-top right of the view which on click will trigger the close() action", () => {
+  /* ToDo developer needs to complete unit test 
+               the notifications view has a close icon located at the very-top right of the view which on click will trigger the close() action
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})

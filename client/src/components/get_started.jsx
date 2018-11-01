444fc7e2c5d48d8ee0c908b43ead358125707d42
Elly <elly@mg.crane.ai>
2018-10-31 22:20:28 -0400

    added client/src/components/get_started refactoring

---
+++ client/src/components/get_started.jsx
@@ -0,0 +1,83 @@
+
+/*
+get started view
+created by: FusionCode
+
+//----------------------------------------------------------------------------------------------------------
+// Component Requirements
+//----------------------------------------------------------------------------------------------------------
+
+the get started view has a status bar located at the very-top across the full width of the view
+get started view has an Image located at the very-top across the full width of the view. this is a placeholder and should be updated with higher resolution Image
+get started view has a Label located at the main area left of the view with text axe/me strea
+
+*/
+        
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Row, Col   } from 'reactstrap';
+        
+        
+        import {  }  from '../actions/get_startedActions.js'; 
+
+        
+        
+       
+        class getstartedView extends React.Component {
+
+            state = {
+                
+                
+            }
+    
+                
+            
+
+            componentDidMount = () => {
+                
+               
+                
+            }
+    
+        render() {
+        
+        
+        const { data } = this.props
+
+            return (
+                <Row>
+
+                 
+                    <Col md={12}>
+
+                        			<Col md={12} className="text-center">				{/* <!-- TODO: Change placeholder image with higher res --> */ }
+				<img className="img-fluid" alt="Responsive image" src='http://path.to/replace/image' /> 
+
+			</Col>			<Col md={12}>
+				<p className="label-block-very-light-yellow"> axe/me strea </p>
+			</Col>
+
+
+                           
+   
+                    </Col> 
+                     
+                </Row>
+            )   
+        }
+    }
+                   
+    function mapStateToProps(state) {
+            
+            }
+        }
+    
+        function mapDispatchToProps(dispatch) {
+            return bindActionCreators({   }, dispatch);
+        }
+    
+   
+        export default connect(mapStateToProps, mapDispatchToProps)(getstartedView);
+    
+        

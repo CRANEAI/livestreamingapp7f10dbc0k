7b407e3e0db8472ff9677f25f73451f4a0faac43
Elly <elly@mg.crane.ai>
2018-10-31 23:17:24 -0400

    added tests/feed updated project details in new api

---
+++ tests/feed.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - feed UNIT TESTS
+ * Application Client
+ * Live Streaming App
+*/
+
+/* the exploration view has a close icon located at the very-top right of the view which on click will trigger the close() action */
+it("the exploration view has a close icon located at the very-top right of the view which on click will trigger the close() action", () => {
+  /* ToDo developer needs to complete unit test 
+               the exploration view has a close icon located at the very-top right of the view which on click will trigger the close() action
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})

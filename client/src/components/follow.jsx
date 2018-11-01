e90631ab7926988d8234c0fdda580dae1b309145
Elly <elly@mg.crane.ai>
2018-10-31 23:21:52 -0400

    updated client/src/components/follow updated project details in new api

--- client/src/components/follow.jsx
+++ client/src/components/follow.jsx
@@ -7,18 +7,14 @@ created by: FusionCode
 // Component Requirements
 //----------------------------------------------------------------------------------------------------------
 
-the sign up view has a status bar located at the very-top across the full width of the view
-the sign up view has a Headline Label located at the top left of the view, correct text needs defined
-sign up view has Label located at the top left of the view, text needs identified
-sign up view has Label located at the top left of the view, text needs identified
-sign up view has Label located at the top left of the view, text needs identified
-sign up view has Label located at the mid-top left of the view, text needs identified
-sign up view has Label located at the mid-top left of the view, text needs identified
-sign up view has Label located at the main area left of the view, text needs identified
-the sign up view has a Pager located at the main area left of the view, correct text needs defined
-sign up view has Label located at the bottom center of the view, text needs identified
-sign up view has Label located at the bottom center of the view, text needs identified
-view has actions which trigger on events, view should should error messages and display activity indicator when processing
+the follow view has a status bar located at the very-top across the full width of the view
+follow view has Navigation Bar located at the very-top in the center of the view with text follow
+follow view has an Image located at the very-top center of the view. this is a placeholder and should be updated with higher resolution Image
+follow view has an Image located at the very-top left of the view. this is a placeholder and should be updated with higher resolution Image
+follow view has a Label located at the top center of the view with text {a
+follow view has an Image located at the top center of the view. this is a placeholder and should be updated with higher resolution Image
+follow view has an Image located at the top left of the view. this is a placeholder and should be updated with higher resolution Image
+follow view has a Label located at the main area left of the view with text a _
 
 */
         

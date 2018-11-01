6ffea260a59f0d9ba4771807893472325ad50744
Elly <elly@mg.crane.ai>
2018-10-31 22:25:31 -0400

    added tests/get_started refactoring

---
+++ tests/get_started.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - get started UNIT TESTS
+ * Application Client
+ * Live Streaming App
+*/
+
+/* get started view has a Label located at the main area left of the view with text axe/me strea */
+it("get started view has a Label located at the main area left of the view with text axe/me strea", () => {
+  /* ToDo developer needs to complete unit test 
+               get started view has a Label located at the main area left of the view with text axe/me strea
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})

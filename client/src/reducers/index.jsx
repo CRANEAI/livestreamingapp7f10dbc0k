b260b2837ffea5b7dc92b98a85f96311903365dd
Elly <elly@mg.crane.ai>
2018-11-01 01:07:00 -0400

    updated client/src/reducers/index updated project details in new api

--- client/src/reducers/index.jsx
+++ client/src/reducers/index.jsx
@@ -2,30 +2,32 @@
         import { combineReducers }                      from 'redux';
         import { reducer as Form }                      from 'redux-form';
 
-        import notifications from './reducer_notifications' 
+        import recommendations from './reducer_recommendations' 
+import live from './reducer_live' 
 import "aw-r_m from './reducer_"aw-r_m' 
 import sign_up from './reducer_sign_up' 
 import search from './reducer_search' 
 import chat from './reducer_chat' 
 import get_started from './reducer_get_started' 
 import edit_profile from './reducer_edit_profile' 
-import share from './reducer_share' 
 import follow from './reducer_follow' 
 import composite from './reducer_composite' 
-import live from './reducer_live'
+import share from './reducer_share' 
+import sign_up from './reducer_sign_up'
 
         const rootReducer = combineReducers({
-            notifications : notifications 
+            recommendations : recommendations 
+,live : live 
 ,"aw-r_m : "aw-r_m 
 ,sign_up : sign_up 
 ,search : search 
 ,chat : chat 
 ,get_started : get_started 
 ,edit_profile : edit_profile 
-,share : share 
 ,follow : follow 
 ,composite : composite 
-,live : live 
+,share : share 
+,sign_up : sign_up 
 
         })
 
